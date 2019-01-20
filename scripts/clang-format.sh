7440652a4e4461154742781a8a9fa4968dcc6c71
Andrei Maximov <andrei.maximov@gmail.com>
2019-01-19 16:56:49 -0800

    Include examples in clang-format.sh script

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -5,5 +5,6 @@ find                                    \
     "${MESON_SOURCE_ROOT}/include/unet" \
     "${MESON_SOURCE_ROOT}/test"         \
     "${MESON_SOURCE_ROOT}/bench"        \
+    "${MESON_SOURCE_ROOT}/examples"     \
     -iname '*.cpp' -o -iname '*.hpp' | xargs clang-format -i -style=file
 
